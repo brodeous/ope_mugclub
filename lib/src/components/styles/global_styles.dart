794a0c86624e78c830364a338b5d58ac1aab2dbe
Noah Meeker <noahmeeker@gmail.com>
2023-01-10 23:28:47 -0800

    added text styles

--- lib/src/components/styles/global_styles.dart
+++ lib/src/components/styles/global_styles.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
 
 class Styles {
+  static TextStyle primaryHeader = TextStyle(
+    fontSize: 32,
+  );
 
-    static TextStyle primaryHeader = TextStyle(
-        fontSize: 32,
-    );
+  static TextStyle secondaryHeader = TextStyle(
+    fontSize: 24,
+  );
 
-    static TextStyle secondaryHeader = TextStyle(
-        fontSize: 24,
-    );
+  static TextStyle displayVisits = TextStyle(
+    fontSize: 64,
+  );
 
-    static TextStyle displayVisits = TextStyle(
-        fontSize: 64,
-    );
+  static TextStyle tileVisits = TextStyle(
+    fontSize: 36,
+  );
 }
