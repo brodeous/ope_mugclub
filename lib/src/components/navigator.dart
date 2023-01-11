2392e432e59da636805bee2e7488ed03ed4b16ea
Noah Meeker <noahmeeker@gmail.com>
2023-01-10 23:31:31 -0800

    Added navigation side bar

---
+++ lib/src/components/navigator.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:ope_mugclub/src/components/qr_scanner.dart';
+import 'package:ope_mugclub/src/components/styles/global_styles.dart';
+import 'package:ope_mugclub/src/pages/member_list.dart';
+
+import '../backend/server.dart';
+
+class NavBar extends StatelessWidget {
+  const NavBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Colors.teal.shade50,
+      child: ListView(
+        children: [
+          Card(
+            clipBehavior: Clip.hardEdge,
+            child: InkWell(
+              splashColor: Colors.teal.withAlpha(100),
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        QRScanner(database: Server.database, version: 0),
+                  ),
+                );
+              },
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                child: Text(
+                  'Add Member',
+                  style: Styles.secondaryHeader,
+                ),
+              ),
+            ),
+          ),
+          Card(
+            clipBehavior: Clip.hardEdge,
+            child: InkWell(
+              splashColor: Colors.teal.withAlpha(100),
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) =>
+                        QRScanner(database: Server.database, version: 1),
+                  ),
+                );
+              },
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                child: Text(
+                  'Returning Member',
+                  style: Styles.secondaryHeader,
+                ),
+              ),
+            ),
+          ),
+          Card(
+            clipBehavior: Clip.hardEdge,
+            child: InkWell(
+              splashColor: Colors.teal.withAlpha(100),
+              onTap: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => const MemberList(),
+                  ),
+                );
+              },
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                child: Text(
+                  'Member List',
+                  style: Styles.secondaryHeader,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
