4383a75cd5a2c8e7f85d92b22db3e748574e54a6
Noah Meeker <noahmeeker@gmail.com>
2023-01-14 22:37:47 -0800

    Updated look, Added UI response to confirmation

--- lib/src/pages/member_info_confirm_page.dart
+++ lib/src/pages/member_info_confirm_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_database/firebase_database.dart';
+import 'package:ope_mugclub/src/components/styles/global_styles.dart';
 import '../models/user.dart';
+import '../backend/server.dart';
 
 class MemberInfoConfirmPage extends StatefulWidget {
   final String? first;
@@ -17,16 +18,14 @@ class MemberInfoConfirmPage extends StatefulWidget {
 }
 
 class _MemberInfoConfirmPageState extends State<MemberInfoConfirmPage> {
-  final database = FirebaseDatabase.instance.ref();
   String first = 'first';
   String last = 'last';
   String email = 'email';
   String phone = 'phone';
   bool stringFixed = false;
+  bool _infoConfirmed = false;
   @override
   Widget build(BuildContext context) {
-    final memberInfoRef = database.child('memberData/');
-
     if (!stringFixed) {
       _fixString();
     }
@@ -36,33 +35,104 @@ class _MemberInfoConfirmPageState extends State<MemberInfoConfirmPage> {
         title: const Text('Confirm Info'),
       ),
       body: Center(
-        child: Column(
-          children: <Widget>[
-            Text('Name:  $first $last'),
-            Text('Email: $email'),
-            Text('Phone: $phone'),
-            Text('QR Code: ${widget.qrCode}'),
-            ElevatedButton(
-              onPressed: () {
-                User newUser = User(
-                    first: first,
-                    last: last,
-                    email: email,
-                    phone: phone,
-                    qrCode: '${widget.qrCode}');
-                memberInfoRef
-                    .child('${widget.qrCode}/')
-                    .set(newUser.toDict())
-                    .then((_) => debugPrint('Member\'s info written'))
-                    .catchError((error) =>
-                        debugPrint('Error writting to Database! $error'));
-              },
-              child: const Text('Confirm Member\'s Info'),
-            ),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.only(
+            top: 100,
+          ),
+          child: ListView(
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  'Name:  $first $last',
+                  style: Styles.secondaryHeader,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  'Email: $email',
+                  style: Styles.secondaryHeader,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  'Phone: $phone',
+                  style: Styles.secondaryHeader,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 5,
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  'QR Code: ${widget.qrCode}',
+                  style: Styles.secondaryHeader,
+                ),
+              ),
+              _response(),
+            ],
+          ),
         ),
       ),
     );
+  }
+
+  Widget _response() {
+    if (!_infoConfirmed) {
+      return ElevatedButton(
+        style: Styles.buttonStyle,
+        onPressed: () {
+          User newUser = User(
+              first: first,
+              last: last,
+              email: email,
+              phone: phone,
+              qrCode: '${widget.qrCode}');
+          Server.database
+              .child('${widget.qrCode}/')
+              .set(newUser.toDict())
+              .then((_) {
+            debugPrint('Member\'s info written');
+            setState(() {
+              _infoConfirmed = true;
+            });
+          }).catchError(
+                  (error) => debugPrint('Error writting to Database! $error'));
+        },
+        child: const Text('Confirm Info'),
+      );
+    } else {
+      return Container(
+        alignment: Alignment.center,
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          color: Colors.green,
+        ),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 10,
+        ),
+        child: const Text(
+          'Member Added',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+          ),
+        ),
+      );
+    }
   }
 
   void _fixString() {
