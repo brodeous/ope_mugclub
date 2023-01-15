89b9ecac86991570ed83561499d93d2248b7c601
Noah Meeker <noahmeeker@gmail.com>
2023-01-14 22:35:33 -0800

    Added Styles

--- lib/src/components/styles/global_styles.dart
+++ lib/src/components/styles/global_styles.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
 
 class Styles {
-  static TextStyle primaryHeader = TextStyle(
+  static TextStyle homepageHeader = const TextStyle(
+    fontSize: 60,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle primaryHeader = const TextStyle(
     fontSize: 32,
   );
 
-  static TextStyle secondaryHeader = TextStyle(
+  static TextStyle secondaryHeader = const TextStyle(
     fontSize: 24,
   );
 
-  static TextStyle displayVisits = TextStyle(
+  static TextStyle displayVisits = const TextStyle(
     fontSize: 64,
+    color: Colors.white,
   );
 
-  static TextStyle tileVisits = TextStyle(
+  static TextStyle tileVisits = const TextStyle(
     fontSize: 36,
+  );
+
+  static ButtonStyle buttonStyle = ElevatedButton.styleFrom(
+    fixedSize: const Size.fromWidth(200),
   );
 }
