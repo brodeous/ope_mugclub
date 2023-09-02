9bf01784f40062cf3a85f4dfb4a5af7a5f20c9d7
Noah Meeker <73568738+NMeekGit@users.noreply.github.com>
2023-09-01 17:40:08 -0700

    removed uneeded class

--- lib/src/utils/firebase_methods.dart
+++ lib/src/utils/firebase_methods.dart
@@ -4,12 +4,10 @@ import 'package:firebase_core/firebase_core.dart';
 import '../models/user.dart';
 import 'package:ope_mugclub/src/backend/server.dart';
 
-class FireMethods {
 
-    static void updateVisits({required int visits, required String qrCode, required DatabaseReference database}) {
-        database.update({
-            "$qrCode/visits" : '$visits',
-        }).then((_) => debugPrint('visits updated'))
-        .catchError((error) => debugPrint('visits update error! $error'));
-    }
+void updateVisits({required int visits, required String qrCode, required DatabaseReference database}) {
+    database.update({
+        "$qrCode/visits" : '$visits',
+    }).then((_) => debugPrint('visits updated'))
+    .catchError((error) => debugPrint('visits update error! $error'));
 }
